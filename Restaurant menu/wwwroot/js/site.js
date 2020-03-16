c90962cec58c53147f56c72a78f71843d917c2d0
Koan Miki <aomiki.iki@gmail.com>
2020-03-16 20:03:43 +0400

    filtraition inputs without behaviour

--- Restaurant menu/wwwroot/js/site.js
+++ Restaurant menu/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+document.getElementById('search').addEventListener('click', ShowElement, false);
+function ShowElement()
+{
+    const curDisplayState = document.getElementById('SearchFields').style.display;
+    if (curDisplayState == 'none' || curDisplayState == '')
+    {
+        document.getElementById('SearchFields').style.display = 'table-row';
+    } else {
+        document.getElementById('SearchFields').style.display = 'none';
+    }
+}
