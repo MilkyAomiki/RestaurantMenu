059b37018f3ef367320dbc2d69c5df735ae98411
Koan Miki <aomiki.iki@gmail.com>
2020-03-22 21:59:19 +0400

    Better filtration

--- Restaurant menu/Controllers/FilterController.cs
+++ Restaurant menu/Controllers/FilterController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestaurantMenu.BLL.DTO;
 using RestaurantMenu.BLL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -35,7 +38,64 @@ namespace Restaurant_menu.Controllers
         [HttpPost]
         public IEnumerable<short> Filtration(FieldTypes name, string filter)
         {
-            return _filtration.FilterOut(name, filter);
+            var kk = HttpContext.Request.Form.Keys;
+            var filters = new Dictionary<FieldTypes, string>();
+            try
+            {
+                JArray array = JArray.Parse(kk.ElementAt(0));
+
+                foreach (var item in array)
+                {
+                    var key = item.SelectToken("name").ToString();
+                    var value = item.SelectToken("value").ToString();
+
+                    filters.Add(GetFieldType(key), value);
+
+                    //foreach (var it in (JObject)item)
+                    //{
+                        
+                    //    var k = it.Key;
+                    //    var v = it.Value;
+                    //}   
+                }
+                
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            var idofitems = new List<short>();
+            foreach (var f in filters)
+            {
+                if (idofitems.Count == 0)
+                {
+                    idofitems = _filtration.FilterOut(f.Key, f.Value).ToList();
+                }
+                else
+                {
+                    idofitems = _filtration.FilterOut(f.Key, f.Value).Intersect(idofitems).ToList();
+                }
+                
+            }
+
+            return idofitems;
         }
+
+        public FieldTypes GetFieldType(string stringType)
+        {
+            return stringType switch
+            {
+                "Name" => FieldTypes.Name,
+                "CreateDate" => FieldTypes.CreateDate,
+                "Consistence" => FieldTypes.Consistence,
+                "Description" => FieldTypes.Description,
+                "Price" => FieldTypes.Price,
+                "Gram" => FieldTypes.Gram,
+                "Calorific" => FieldTypes.Calorific,
+                "CookTime" => FieldTypes.CookTime,
+                _ => 0
+            };
+        }
+
     }
 }
--- Restaurant menu/wwwroot/js/site.js
+++ Restaurant menu/wwwroot/js/site.js
@@ -5,11 +5,9 @@
 
 
 document.getElementById('search').addEventListener('click', ShowElement, false);
-function ShowElement()
-{
+function ShowElement() {
     const curDisplayState = document.getElementById('SearchFields').style.display;
-    if (curDisplayState == 'none' || curDisplayState == '')
-    {
+    if (curDisplayState == 'none' || curDisplayState == '') {
         document.getElementById('SearchFields').style.display = 'table-row';
     } else {
         document.getElementById('SearchFields').style.display = 'none';
@@ -22,10 +20,10 @@ $('#SearchFields').children('th').children('input').each(function () {
     $(this).keyup(function (e) {
 
         var fieldsList = new Array();
-        
+
         for (var i in $('#SearchFields').children('th').children('input')) {
 
-            
+
             if (i == "length") {
                 Filter(fieldsList);
                 return false;
@@ -39,7 +37,7 @@ $('#SearchFields').children('th').children('input').each(function () {
                 var field = { name: fieldname, value: fieldvalue }
                 fieldsList.push(field);
             }
-            
+
         }
 
     });
@@ -48,41 +46,41 @@ function Filter(fieldsList) {
 
     /* If list of fields is empty, show all dishes */
     if (fieldsList.length == 0) {
-        $('tbody').children('tr').each(function () { $(this).show(); $('#SearchFields').css('display', 'table-row');})
+        $('tbody').children('tr').each(function () { $(this).show(); $('#SearchFields').css('display', 'table-row'); })
     } else {
 
         /* For each element in list of fields hide elements */
-        for (var i = 0; i < fieldsList.length; i++) {
+
+        $.post("/Filter/Filtration", JSON.stringify(fieldsList), function (result) {
+            console.log(result);
+            $('#Filtred').text(result.length);
+            var ItemsCount = 0;
+            $('tbody').children('tr').each(function () {
+                var matched = false;
 
 
-            $.post("/Filter/Filtration?name=" + fieldsList[i].name + "&filter=" + fieldsList[i].value, null, function (result) {
-                console.log(result);
-                $('#Filtred').text(result.length);
-                var ItemsCount = result.length;
-                $('tbody').children('tr').each(function () {
-                    var matched = false;
+                for (var i = 0; i < result.length; i++) {
+                    if ($(this).attr('id') == result[i]) {
+                        matched = true;
+                        ItemsCount++;
+                        break;
 
-                 
-                    for (var i = 0; i < result.length; i++) {
-                        if ($(this).attr('id') == result[i]) {
-                            matched = true;
-                            console.log("<b> "+$(this).attr('name') + " is matched " + "</b>");
-                            break;
-                    
-                        }
-                    } if (matched == true) {
-                        if ($(this).is(":hidden")) {
-                            ItemsCount--;
-                            $('#Filtred').text(ItemsCount);
-                        }
                     }
-                    if (matched == false) {
-                        $(this).hide();
+                }
+                if (matched == true) {
+                    if ($(this).is(":hidden")) {
+                        $(this).show();
                         $('#SearchFields').css('display', 'table-row');
-
                     }
-                })
+                }
+                if (matched == false) {
+                    $(this).hide();
+                    $('#SearchFields').css('display', 'table-row');
+
+                }
             })
-        }
+            $('#Filtred').text(ItemsCount);
+        }, "json")
+
     }
 }
